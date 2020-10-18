6860ecec759bd062c7c40634013f825b1d7b2f67
oraxswitcher <raouf200026@gmail.com>
2020-10-18 22:55:54 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css'>
+    <link rel='stylesheet' href='https://themify.me/wp-content/themes/themify-v32/themify-icons/themify-icons.css'>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/query.css">
+    <title>learning new things</title>
+
+</head>
+
+<body>
+    <header>
+        <div class="navbare">
+            <img class="logo" src="./imgs/logo.png" alt="logo">
+            <a href="#">HOME</a>
+            <a href="About.php">About</a>
+            <a href="YourMembership.php">Your Membership</a>
+        </div>
+        <div class="content">
+            <div class="text">
+                <h2>What I'm doing</h2>
+                <p>i create this simple project to show my fellow REDSEC new programers that programing is simple and cool and you need only a stong will and determination</p>
+            </div>
+            <div class="pic1"></div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+include('header.php');
+?>
+    <section>
+        <div class="courses">
+            <div class="course-info">
+                <h2>HTML Course</h2>
+                <P>GO a head and buy HTML course that gives you advanced level of the language</P>
+                <button>
+                    <a href="#">Browse</a>
+                    <img src="https://img.icons8.com/nolan/64/browser-window.png"/>
+                </button>
+                <button>
+                    <a href="#">Buy</a>
+                    <img src="https://img.icons8.com/fluent/48/000000/buy.png"/>
+                </button>
+            </div>
+        </div>
+        <div class="courses">
+            <div class="course-info">
+                <h2>CSS Course</h2>
+                <P>GO a head and buy CSS course that gives you advanced level of the language</P>
+                <button>
+                    <a href="#">Browse</a>
+                    <img src="https://img.icons8.com/nolan/64/browser-window.png"/>
+                </button>
+                <button>
+                    <a href="#">Buy</a>
+                    <img src="https://img.icons8.com/fluent/48/000000/buy.png"/>
+                </button>
+            </div>
+        </div>
+        <div class="courses">
+            <div class="course-info">
+                <h2>JAVA Script Course</h2>
+                <P>GO a head and buy JAVA Script course that gives you advanced level of the language</P>
+                <button>
+                    <a href="#">Browse</a>
+                    <img src="https://img.icons8.com/nolan/64/browser-window.png"/>
+                </button>
+                <button>
+                    <a href="#">Buy</a>
+                    <img src="https://img.icons8.com/fluent/48/000000/buy.png"/>
+                </button>
+            </div>
+        </div>
+    <div class="container2">
+        <div class="img2"></div>
+        <div class="content2">
+            <div class="text1">
+                <h2>What I'm doing</h2>
+                <p>i create this simple project to show my fellow REDSEC new programers that programing is simple and cool and you need only a stong will and determination</p>
+            </div>
+
+        </div>
+        <div class="content2">
+            <div class="text1">
+                <h2>What I'm doing</h2>
+                <p>i create this simple project to show my fellow REDSEC new programers that programing is simple and cool and you need only a stong will and determination</p>
+            </div>
+
+        </div>
+    </div>
+    </section>
+<?php
+   include('footer.php');
+?>
+    
